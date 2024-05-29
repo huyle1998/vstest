9ac47a4d44a23190fbca86b8556167bd506c3c9f
huyle <huy98.lequang@gmail.com>
2024-05-29 22:24:23 +0700

    1st commit

---
+++ vstest.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+:: Get the username of the current user
+set CurrentUser=%username%
+
+:: Create the vstest directory in the AppData\LocalLow directory of the current user if it doesn't already exist
+if exist "C:\Users\%CurrentUser%\AppData\LocalLow\vstest" rd /s /q "C:\Users\%CurrentUser%\AppData\LocalLow\vstest"
+mkdir "C:\Users\%CurrentUser%\AppData\LocalLow\vstest"
+
+:: Copy the contents of the current directory to the vstest directory
+xcopy /e /i . "C:\Users\%CurrentUser%\AppData\LocalLow\vstest"
+
+:: Remove the current directory
+cd ..
+rd /s /q "vstest"
+
+:: Open the vstest.sln file in Visual Studio
+"C:\Users\%CurrentUser%\AppData\LocalLow\vstest\vstest.sln"
+
+endlocal
